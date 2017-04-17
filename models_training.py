de7d84433404eceed1e82b5b3424400f42ec7cfe
Shriya Atmakuri <shriya.atmak@gmail.com>
2017-04-17 19:50:01 +0530

    Create features updated to cumulative. Models updated for variable columns

--- models_training.py
+++ models_training.py
@@ -11,11 +11,11 @@ from sklearn.metrics import classification_report, make_scorer
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 
-def load_csv(file_name):
-	reader = csv.reader(open(file_name, "rt", encoding="utf8"))
-	dataset = list(reader)
+'''Enter required features as single string. No spaces'''
+
+def load_csv(file_name, choose_cols):
 	dataset = pd.read_csv (file_name, header=None)
-	dataset = dataset.values.tolist()
+	dataset = dataset.values
 	rows = len(dataset)
 	cols = len(dataset[0])
 	feature_data = []
@@ -25,7 +25,7 @@ def load_csv(file_name):
 	for r in range (0, rows):
 		for c in range (colStart, colEnd):
 			dataset[r][c] = float(dataset[r][c])
-		feature_data.append (dataset[r][colStart:colEnd])
+		feature_data.append (dataset[r][choose_cols])
 		label_data.append (int(dataset[r][cols-1]))
 	'''print (dataset[0])
 	print (cols)
@@ -62,7 +62,6 @@ def ml_k_nearest_neighbours(features_train, features_test, labels_train, labels_
 	print("KNN Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 	model.fit(features_train, labels_train)
 	predictions = model.predict(features_test)
-	print (features_test[15], labels_test[15], predictions[15])
 	print (classification_report(labels_test, predictions))
 	print (confusion_matrix(labels_test, predictions))
 	print (accuracy_score(labels_test, predictions))
@@ -87,11 +86,33 @@ def ml_decision_tree(features_train, features_test, labels_train, labels_test):
 	print (confusion_matrix(labels_test, predictions))
 	print (accuracy_score(labels_test, predictions))
 
-file_name = './new_output_featuresnohh.csv' #input ('Enter the path of features.csv: ')
-features,labels = load_csv(file_name)
+file_name = './output_features_hh.csv' #input ('Enter the path of features.csv: ')
 
+print('1: Texture 2: Hue Histogram 3: Contrast 4: Coherence 5: Hue Count 6: Sobel Otsu')
+print('Choose subset:')
+in_string=input()
+choose_cols=[]
+for num in in_string:
+	num=int(num)
+	if num==1:
+		for item in range(6):
+			choose_cols.append(item)
+	elif num==2:
+		choose_cols.append(6)
+	elif num==3:
+		choose_cols.append(7)
+	elif num==4:
+		choose_cols.append(8)
+	elif num==5:
+		choose_cols.append(9)
+	elif num==6:
+		for item in range(10,35):
+			choose_cols.append(item)
+print(choose_cols)
+
+features,labels = load_csv(file_name, choose_cols)
 features_train, features_test, labels_train, labels_test = make_training_and_testing(features, labels)
 ml_logistic_regression(features_train, features_test, labels_train, labels_test)
 ml_k_nearest_neighbours(features_train, features_test, labels_train, labels_test)
-ml_support_vector_machine(features_train, features_test, labels_train, labels_test)
+#ml_support_vector_machine(features_train, features_test, labels_train, labels_test)
 ml_decision_tree(features_train, features_test, labels_train, labels_test)
